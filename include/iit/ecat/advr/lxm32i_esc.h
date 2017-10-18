0eebb0e7de6730e275c2595438e2fda9c5cfcc0e
Alessio Margan <alessio.margan@iit.it>
2017-10-18 20:11:24 +0200

    jog/pos/vel/tor profile working

--- include/iit/ecat/advr/lxm32i_esc.h
+++ include/iit/ecat/advr/lxm32i_esc.h
@@ -18,43 +18,118 @@
 #include <map>
 #include <iostream>
 
+
 namespace iit {
 namespace ecat {
 namespace advr {
+
+#define _MK_STR(a) static const std::string a ( #a )
+
+//static const std::string _DCOMstatus( "_DCOMstatus");
+_MK_STR(_DCOMstatus);
+_MK_STR(_DCOMopmd_act);
+_MK_STR(_p_act);
+_MK_STR(_p_dif);
+_MK_STR(_tq_act);
+_MK_STR(_LastError);
+_MK_STR(_IO_act);
+
+
+std::vector<std::string> const rd_pdos = { _DCOMstatus, _DCOMopmd_act, _p_act, _p_dif, _tq_act, _LastError, _IO_act };
+std::vector<std::string> const rd_sdos = { };
+
+_MK_STR(DCOMcontrol);
+_MK_STR(DCOMopmode);
+_MK_STR(PPp_target);
+_MK_STR(PVv_target);
+_MK_STR(PTtq_target);
+_MK_STR(IO_DQ_set);
+_MK_STR(JOGactivate);
+
+_MK_STR(PPv_target);
+_MK_STR(RAMP_v_acc);
+_MK_STR(RAMP_v_dec);
+_MK_STR(ScalePOSdenom);
+_MK_STR(ScalePOSnum);
+
+
+std::vector<std::string> const wr_pdos = { DCOMcontrol, DCOMopmode, PPp_target, PVv_target, PTtq_target, IO_DQ_set, JOGactivate };
+std::vector<std::string> const wr_sdos = { PPv_target, RAMP_v_acc, RAMP_v_dec};
+
+_MK_STR(RxPdoMapCnt);
+_MK_STR(RxPdoMap);
+_MK_STR(RPdo1);
+_MK_STR(RPdo2);
+_MK_STR(RPdo3);
+_MK_STR(RPdo4);
+_MK_STR(RPdo5);
+_MK_STR(RPdo6);
+_MK_STR(RPdo7);
+_MK_STR(RPdo8);
+std::vector<std::string> const rpdos = { RPdo1, RPdo2, RPdo3, RPdo4, RPdo5, RPdo6, RPdo7, RPdo8 };
+
+
+_MK_STR(TxPdoMapCnt);
+_MK_STR(TxPdoMap);
+_MK_STR(TPdo1);
+_MK_STR(TPdo2);
+_MK_STR(TPdo3);
+_MK_STR(TPdo4);
+_MK_STR(TPdo5);
+_MK_STR(TPdo6);
+_MK_STR(TPdo7);
+_MK_STR(TPdo8);
+std::vector<std::string> const tpdos = { TPdo1, TPdo2, TPdo3, TPdo4, TPdo5, TPdo6, TPdo7, TPdo8 };
+
+
+std::vector<std::string> const pdo_map = { RxPdoMapCnt, RxPdoMap, TxPdoMapCnt, TxPdoMap };
+
 
 #define DUMPWNAME(os, name, a, d) \
     do { (os) << (name) << "=" << (a) << d; } while(0)
         
 #define DUMP(os, a, d) DUMPWNAME((os), #a, (a), (d))  
 
-struct CTRL_STATUS {
+        
+// Bits 0, 1, 2, 3, and 5, 6 of the DCOMstatus parameter provide information on the operating state.
+        
+struct DCOM_STATUS {
 
-    uint16_t  status_bits1:4;
-    uint16_t  voltage_enabled:1;
-    uint16_t  status_bits2:2;
-    uint16_t  warning:1;
-    uint16_t  halt_request_active:1;
-    uint16_t  remote:1;
-    uint16_t  target_reached:1;
-    uint16_t  bit11:1;
-    uint16_t  operating_mode_specific:1;
-    uint16_t  x_err:1;
-    uint16_t  x_end:1;
-    uint16_t  ref_ok:1;
+    uint16_t  b0_ReadyToSwitchOn:1;
+    uint16_t  b1_SwitchedOn:1;
+    uint16_t  b2_OperationEnabled:1;
+    uint16_t  b3_Fault:1;
+    uint16_t  b4_VoltageEnabled:1;
+    uint16_t  b5_QuickStop:1;
+    uint16_t  b6_SwtchedOnDisabled:1;
+    uint16_t  b7_warning:1;
+    uint16_t  b8_halt_request_active:1;
+    uint16_t  b9_remote:1;
+    uint16_t  b10_target_reached:1;
+    uint16_t  b11:1;
+    uint16_t  b12_operating_mode_specific:1;
+    uint16_t  b13_x_err:1;
+    uint16_t  b14_x_end:1;
+    uint16_t  b15_ref_ok:1;
     
     std::ostream& dump ( std::ostream& os, const std::string delim ) const {
     
-        DUMP( os, status_bits1, delim );
-        DUMP( os, voltage_enabled, delim );
-        DUMP( os, status_bits2, delim );
-        DUMP( os, warning, delim );
-        DUMP( os, remote, delim );
-        DUMP( os, target_reached, delim );
-        DUMP( os, bit11, delim );
-        DUMP( os, operating_mode_specific, delim );
-        DUMP( os, x_err, delim );
-        DUMP( os, x_end, delim );
-        DUMP( os, ref_ok, delim );
+        DUMP( os, b0_ReadyToSwitchOn, delim );
+        DUMP( os, b1_SwitchedOn, delim );
+        DUMP( os, b2_OperationEnabled, delim );
+        DUMP( os, b3_Fault, delim );
+        DUMP( os, b4_VoltageEnabled, delim );
+        DUMP( os, b5_QuickStop, delim );
+        DUMP( os, b6_SwtchedOnDisabled, delim );
+        DUMP( os, b7_warning, delim );
+        DUMP( os, b8_halt_request_active, delim );
+        DUMP( os, b9_remote, delim );
+        DUMP( os, b10_target_reached, delim );
+        DUMP( os, b11, delim );
+        DUMP( os, b12_operating_mode_specific, delim );
+        DUMP( os, b13_x_err, delim );
+        DUMP( os, b14_x_end, delim );
+        DUMP( os, b15_ref_ok, delim );
         return os;
         
     }
@@ -69,67 +144,150 @@ struct CTRL_STATUS {
         dump(oss,"\t");
         return snprintf ( buff, size, "%s", oss.str().c_str() );
     }
-        
+       
 };
+
+// Bits 0, 1, 2, 3 and 7 of the parameter DCOMcontrol allow you to switch between the operating states.
+
+struct DCOM_CONTROL {
+
+    uint16_t  b0_SwitchOn:1;
+    uint16_t  b1_EnableVoltage:1;
+    uint16_t  b2_QuickStop:1;
+    uint16_t  b3_EnableOperation:1;
+    uint16_t  b4_:1;
+    uint16_t  b5_:1;
+    uint16_t  b6_:1;
+    uint16_t  b7_FaultReset:1;
+    uint16_t  b8_Halt:1;
+    uint16_t  b9_ChangeOnSetpoint:1;
+    uint16_t  _reserved:6;
+    
+    std::ostream& dump ( std::ostream& os, const std::string delim ) const {
+    
+        DUMP( os, b0_SwitchOn, delim );
+        DUMP( os, b1_EnableVoltage, delim );
+        DUMP( os, b2_QuickStop, delim );
+        DUMP( os, b3_EnableOperation, delim );
+        DUMP( os, b7_FaultReset, delim );
+        DUMP( os, b8_Halt, delim );
+        DUMP( os, b9_ChangeOnSetpoint, delim );
+        return os;
+        
+    }    
+    void fprint ( FILE *fp ) {
+        std::ostringstream oss;
+        dump(oss,"\t");
+        fprintf ( fp, "%s", oss.str().c_str() );
+    }
+    int sprint ( char *buff, size_t size ) {
+        std::ostringstream oss;
+        dump(oss,"\t");
+        return snprintf ( buff, size, "%s", oss.str().c_str() );
+    }
+};
+
 
 typedef union{
     uint16_t            all;
-    struct CTRL_STATUS  ctrl_status;
-} control_status_t;
-    
+    struct DCOM_CONTROL  dcom_control;
+} dcom_control_t;
 
-struct LXM32iEscPdoTypes {
+typedef union{
+    uint16_t            all;
+    struct DCOM_STATUS  dcom_status;
+} dcom_status_t;
+
+
+class LXM32iEscPdoTypes {
+   
+public:
+    
     // TX  slave_input -- master output
     struct pdo_tx {
-        uint16_t    DCOMcontrol;
-        int32_t     PPp_target;
-        uint16_t    IO_DQ_set;
+        //uint8_t _pdo_tx[64];
+        dcom_control_t  DCOMcontrol;
+        uint8_t         DCOMopmode;
+        int32_t         PPp_target;
+        int32_t         PVv_target;
+        int16_t         PTtq_target;
+        uint16_t        IO_DQ_set;
+        uint16_t        JOGactivate;
+        
     }  __attribute__ ( ( __packed__ ) );
 
     // RX  slave_output -- master input
     struct pdo_rx {
-        control_status_t    _DCOMstatus;
-        int32_t             _p_act;
-        uint16_t            _LastError;
-        uint16_t            _IO_act;
+        //uint8_t _pdo_rx[64];
+        dcom_status_t   _DCOMstatus;
+        uint8_t         _DCOMopmd_act;
+        int32_t         _p_act;
+        int32_t         _p_dif;
+        int32_t         _tq_act;
+        uint16_t        _LastError;
+        uint16_t        _IO_act;
         
-        std::ostream& dump ( std::ostream& os, const std::string delim ) const {
-            _DCOMstatus.ctrl_status.dump(os,",");
-            os << _p_act << delim;
-            os << _LastError << delim;
-            os << _IO_act << delim;
-            //os << std::endl;
-            return os;
-        }
-        void fprint ( FILE *fp ) {
-            std::ostringstream oss;
-            dump(oss,"\t");
-            fprintf ( fp, "%s", oss.str().c_str() );
-        }
-        int sprint ( char *buff, size_t size ) {
-            std::ostringstream oss;
-            dump(oss,"\t");
-            return snprintf ( buff, size, "%s", oss.str().c_str() );
-        }
-        void to_map ( jmap_t & jpdo ) {
-        }
-        void pb_toString( std::string * pb_str ) {
-            // !!! NO static declaration
-            iit::advr::Ec_slave_pdo pb_rx_pdo;
-            static struct timespec ts;
-            clock_gettime(CLOCK_MONOTONIC, &ts);
-            // Header
-            pb_rx_pdo.mutable_header()->mutable_stamp()->set_sec(ts.tv_sec);
-            pb_rx_pdo.mutable_header()->mutable_stamp()->set_nsec(ts.tv_nsec);
-            // Type
-            pb_rx_pdo.set_type(iit::advr::Ec_slave_pdo::RX_SKIN_SENS);
-            //
-            pb_rx_pdo.SerializeToString(pb_str);
-        }
+    std::ostream& dump ( std::ostream& os, const std::string delim ) const {
+        _DCOMstatus.dcom_status.dump(os,delim);
+        os << "0x" << std::hex << _DCOMstatus.all << std::dec << delim;
+        os << "_DCOMopmd_act=" << (int)_DCOMopmd_act << std::dec << delim;
+        //DUMP( os, _DCOMopmd_act, delim );
+        DUMP( os, _p_act, delim );
+        DUMP( os, _p_dif, delim );
+        DUMP( os, _tq_act, delim );
+        DUMP( os, _LastError, delim );
+        DUMP( os, _IO_act, delim );
+        //os << std::endl;
+        return os;
+    }
+    
+    void fprint ( FILE *fp ) {
+        std::ostringstream oss;
+        dump(oss,"\t");
+        fprintf ( fp, "%s", oss.str().c_str() );
+    }
+    int sprint ( char *buff, size_t size ) {
+        std::ostringstream oss;
+        dump(oss,"\t");
+        return snprintf ( buff, size, "%s", oss.str().c_str() );
+    }
+    void pb_toString( std::string * pb_str ) {
+        // !!! NO static declaration
+        iit::advr::Ec_slave_pdo pb_rx_pdo;
+        static struct timespec ts;
+        clock_gettime(CLOCK_MONOTONIC, &ts);
+        // Header
+        pb_rx_pdo.mutable_header()->mutable_stamp()->set_sec(ts.tv_sec);
+        pb_rx_pdo.mutable_header()->mutable_stamp()->set_nsec(ts.tv_nsec);
+        // Type
+        pb_rx_pdo.set_type(iit::advr::Ec_slave_pdo::RX_SKIN_SENS);
+        //
+        pb_rx_pdo.SerializeToString(pb_str);
+    }
 
     }  __attribute__ ( ( __packed__ ) );
 };
 
+typedef enum : uint16_t {
+    STOP = 0,
+    POS_SLOW = 1,
+    NEG_SLOW = 2,
+    POS_FAST = 5,
+    NEG_FAST = 6,
+} jogact_t;
+
+typedef enum : int8_t {
+    AUTO = -6,
+    JOG = -1,
+    PPOS = 1,
+    PVEL = 3,
+    PTOR = 4,
+    HOMING = 6,
+    IPOS = 7,
+    CSPOS = 8,
+    CSVEL = 9,
+    CSTOR = 10,
+} opmode_t;
 
 struct LXM32iEscSdoTypes {
     
@@ -137,23 +295,73 @@ struct LXM32iEscSdoTypes {
     uint16_t    rxPdoMap;
     
     uint8_t     rxElemCnt;
-    uint32_t    rxMap[3];
+    uint32_t    rxMap[8];
     
     uint8_t     txPdoMapCnt;
     uint16_t    txPdoMap;
 
     uint8_t     txElemCnt;
-    uint32_t    txMap[4];
+    uint32_t    txMap[8];
+
+#if 0
+    /////////////////////////////////////////////
+    // TX  slave_input -- master output
+    dcom_control_t  DCOMcontrol;
+    uint16_t        JOGactivate;
+    uint8_t         DCOMopmode;
+    int32_t         PPp_target;
+    uint16_t        IO_DQ_set;
+    // RX  slave_output -- master input
+    dcom_status_t   _DCOMstatus;
+    uint8_t         _DCOMopmd_act;
+    int32_t         _p_act;
+    uint16_t        _LastError;
+    uint16_t        _IO_act;
+#endif
+    
+    uint32_t    PPv_target;
+    uint32_t    RAMP_v_acc;
+    uint32_t    RAMP_v_dec;
+        
+    int32_t     ScalePOSdenom;
+    int32_t     ScalePOSnum;
+    
+   std::ostream& dump ( std::ostream& os, const std::string delim ) const {
+       os << "struct LXM32iEscSdoTypes : ";
+       os << (int)rxPdoMapCnt << delim;
+       os << "0x" << std::hex << rxPdoMap << std::dec << delim;
+       os << (int)txPdoMapCnt << delim;
+       os << "0x" << std::hex << txPdoMap << std::dec << delim;
+       os << PPv_target << delim;
+       os << RAMP_v_acc << delim;
+       os << RAMP_v_dec << delim;
+       
+    }
 
 };
 
-static struct LXM32iEscSdoTypes dflt_sdo = {
-    1, 0x1600,
-    3, 0x60400010, 0x607A0020, 0x30081110,
-    1, 0x1A00,
-    4, 0x60410010, 0x60640020, 0x603F0010, 0x30080110, 
+
+// PDO is suitable for the operating mode JOG 
+static struct LXM32iEscSdoTypes yifu_pdomap = {
+    // RxPdo
+    1,
+    0x1600,
+    //6,
+    //{ 0x60400010, 0x60600008, 0x607A0020, 0x60FF0020, 0x60710010, 0x30081110, 0, 0 },
+    7,
+    { 0x60400010, 0x60600008, 0x607A0020, 0x60FF0020, 0x60710010, 0x30081110, 0x301B0910, 0 }, 
+    // TxPdo
+    1,
+    0x1A00,
+    7,
+    { 0x60410010, 0x60610008, 0x60640020, 0x60F40020, 0x60770010, 0x603F0010, 0x30080110, 0 },
+    //
+    2500, 50000, 50000,
+    //
+    32768, 1
         
 };
+
 
 struct LXM32iLogTypes {
 
@@ -170,6 +378,20 @@ struct LXM32iLogTypes {
     }
 };
 
+
+template <typename T>
+void fprint ( T t, FILE *fp ) {
+    std::ostringstream oss;
+    t.dump(oss,"\t");
+    fprintf ( fp, "%s", oss.str().c_str() );
+}
+template <typename T>
+int sprint ( T t, char *buff, size_t size ) {
+    std::ostringstream oss;
+    t.dump(oss,"\t");
+    return snprintf ( buff, size, "%s", oss.str().c_str() );
+}
+
 /*
  *
  * 
@@ -183,6 +405,7 @@ class LXM32iESC :
 {
 public:
     typedef BasicEscWrapper<LXM32iEscPdoTypes,LXM32iEscSdoTypes>    Base;
+
     typedef PDO_log<LXM32iLogTypes>                                 Log;
 
 public:
@@ -210,10 +433,16 @@ public:
     void print_info ( void );
     int init ( const YAML::Node & root_cfg );
     void handle_fault ( void );
+
+    void test_motor( void );
+
+    std::vector<int32_t> set_points; 
+
     
 private:
     stat_t  s_rtt;
     objd_t * SDOs;
+    
 };
 
 /*
@@ -227,13 +456,12 @@ inline uint32_t LXM32iESC::get_ESC_type() {
     return LXM32I;
 }
 inline void LXM32iESC::on_writePDO ( void ) {
-
 }
 inline void LXM32iESC::on_readPDO ( void ) {
 
-    if ( rx_pdo._LastError ) {
-        handle_fault();
-    }
+//     if ( rx_pdo._DCOMstatus.dcom_status.b3_Fault ) {
+//         handle_fault();
+//     }
 
     if ( _start_log ) {
         Log::log_t log;
@@ -256,12 +484,20 @@ inline void LXM32iESC::print_info ( void ) {
 
 inline int LXM32iESC::init ( const YAML::Node & root_cfg ) {
 
-    sdo = dflt_sdo;
     
     std::string robot_name("void");
     try {
         robot_name = root_cfg["ec_boards_base"]["robot_name"].as<std::string>();
     } catch ( YAML::Exception &e ) {
+        DPRINTF ( "No robot name in config ... %s\n", e.what() );
+    }
+    try {
+        yifu_pdomap.PPv_target = root_cfg["Lxm32i_A"]["PPv_target"].as<uint32_t>();
+        yifu_pdomap.RAMP_v_acc = root_cfg["Lxm32i_A"]["RAMP_v_acc"].as<uint32_t>();
+        yifu_pdomap.RAMP_v_dec = root_cfg["Lxm32i_A"]["RAMP_v_dec"].as<uint32_t>();
+        set_points = root_cfg["Lxm32i_A"]["set_points"].as<std::vector<int32_t>>();
+    } catch ( YAML::Exception &e ) {
+        DPRINTF ( "Catch Exception in %s ... %s\n", __PRETTY_FUNCTION__, e.what() );
     }
 
     uint8_t zero = 0;
@@ -270,26 +506,37 @@ inline int LXM32iESC::init ( const YAML::Node & root_cfg ) {
         init_sdo_lookup(false);
         //readSDO_byname ( "sensor_robot_id" );
         
-        //writeSDO_byname( "RxElemCnt", zero );
-        writeSDO_byname( "Rx1",    sdo.rxMap[0] );
-        writeSDO_byname( "Rx2",    sdo.rxMap[1] );
-        writeSDO_byname( "Rx3",    sdo.rxMap[2] );
-        writeSDO_byname( "RxElemCnt", sdo.rxElemCnt );
-
-        //writeSDO_byname( "TxElemCnt", zero );
-        writeSDO_byname( "Tx1",    sdo.txMap[0] );
-        writeSDO_byname( "Tx2",    sdo.txMap[1] );
-        writeSDO_byname( "Tx3",    sdo.txMap[2] );
-        writeSDO_byname( "Tx4",    sdo.txMap[3] );
-        writeSDO_byname( "TxElemCnt", sdo.txElemCnt );
-
-        //writeSDO_byname( "RxPdoMapCnt", zero );
-        writeSDO_byname( "RxPdoMap",    sdo.rxPdoMap );
-        writeSDO_byname( "RxPdoMapCnt", sdo.rxPdoMapCnt );
+        for ( auto const name : rd_pdos ) { readSDO_byname ( name ); }
+        rx_pdo.dump(std::cout, "\n"); std::cout << "\n\n";
         
-        //writeSDO_byname( "TxPdoMapCnt", zero );
-        writeSDO_byname( "TxPdoMap",    sdo.txPdoMap );        
-        writeSDO_byname( "TxPdoMapCnt", sdo.txPdoMapCnt );
+        for ( auto const name : rd_sdos ) { readSDO_byname ( name ); }
+        for ( auto const name : pdo_map ) { readSDO_byname ( name ); }
+        sdo.dump(std::cout, " "); std::cout << "\n";
+        
+        {
+            int i = 0;
+            for ( auto const name : rpdos ) { writeSDO_byname( name, yifu_pdomap.rxMap[i++] ); } 
+            writeSDO_byname( "RxElemCnt", yifu_pdomap.rxElemCnt );
+        }
+        {
+            int i = 0;
+            for ( auto const name : tpdos ) { writeSDO_byname( name, yifu_pdomap.txMap[i++] ); } 
+            writeSDO_byname( "TxElemCnt", yifu_pdomap.txElemCnt );
+        }
+        
+        writeSDO_byname( "RxPdoMap",    yifu_pdomap.rxPdoMap );
+        writeSDO_byname( "RxPdoMapCnt", yifu_pdomap.rxPdoMapCnt );
+        
+        writeSDO_byname( "TxPdoMap",    yifu_pdomap.txPdoMap );        
+        writeSDO_byname( "TxPdoMapCnt", yifu_pdomap.txPdoMapCnt );
+
+        writeSDO_byname( PPv_target, yifu_pdomap.PPv_target );
+        writeSDO_byname( RAMP_v_acc, yifu_pdomap.RAMP_v_acc );
+        writeSDO_byname( RAMP_v_dec, yifu_pdomap.RAMP_v_dec );
+        
+        writeSDO_byname( ScalePOSdenom, yifu_pdomap.ScalePOSdenom );
+        writeSDO_byname( ScalePOSnum, yifu_pdomap.ScalePOSnum );
+        
         
     } catch ( EscWrpError &e ) {
 
@@ -309,9 +556,22 @@ inline int LXM32iESC::init ( const YAML::Node & root_cfg ) {
 
 }
 
+
+inline void LXM32iESC::test_motor ( void ) {
+
+    std::cout << "Test Motor" << "\n";
+    
+}
+
 inline void LXM32iESC::handle_fault ( void ) {
 
-    //DPRINTF("_oXo_ TODO _oXo_ %d\n", rx_pdo.driverErr);
+    if ( rx_pdo._DCOMstatus.dcom_status.b3_Fault ) {
+        tx_pdo.DCOMcontrol.dcom_control.b7_FaultReset = 1;
+        DPRINTF("[%s]: reset fault 0x%X\n", __FUNCTION__, rx_pdo._LastError);
+    } else {
+        tx_pdo.DCOMcontrol.dcom_control.b7_FaultReset = 0;                            
+    }
+
 }
 
 
--- src/lxm32i_esc.cpp
+++ src/lxm32i_esc.cpp
@@ -1,64 +1,61 @@
 #include <iit/ecat/advr/lxm32i_esc.h>
 
-using namespace iit::ecat;
+//using namespace iit::ecat;
 using namespace iit::ecat::advr;
-
-static const char acName6000[] = "Inputs";
-static const char acName6000_0[] = "Number of Elements";
-
-static const char acName7000[] = "Outputs";
-static const char acName7000_0[] = "Number of Elements";
-static const char acName7000_1[] = "pos_ref";
-static const char acName7000_2[] = "tor_ref";
-static const char acName7000_3[] = "direct_ref";
-static const char acName7000_4[] = "ts";
-
-static const char acName8000[] = "Flash Parameter";
-static const char acName8000_0[] = "Number of Elements";
-static const char acName8000_1[] = "Block control";
-static const char acName8000_2[] = "Num Av Samples";
-static const char acName8000_3[] = "ConfigFlags";
-static const char acName8000_4[] = "Sensor_number";
-static const char acName8000_5[] = "Sensor_robot_id";
-
-static const char acName8001[] = "Parameter";
-static const char acName8001_1[] = "fw_ver";
-static const char acName8001_2[] = "ack_board_faults";
-static const char acName8001_3[] = "flash_params_cmd";
-static const char acName8001_4[] = "flash_params_cmd_ack";
-
-
 
 
 static const iit::ecat::objd_t source_SDOs[] = {
 
-    // 0x1A00 slave TxPDO 
-    {0x6041, 0x0,  DTYPE_UNSIGNED16, 16, ATYPE_RO, "_DCOMstatus",   0 },
-    {0x6064, 0x0,  DTYPE_INTEGER32,  32, ATYPE_RO, "_p_act",        0 },
-    {0x603F, 0x0,  DTYPE_UNSIGNED16, 16, ATYPE_RO, "_LastError",    0 },
-    {0x3008, 0x1,  DTYPE_UNSIGNED16, 16, ATYPE_RO, "_IO_act",       0 },
-    // 0x1600 slave RxPDO 
-    {0x6040, 0x0,  DTYPE_UNSIGNED16, 16, ATYPE_RW, "DCOMcontrol",   0 },
-    {0x607A, 0x0,  DTYPE_INTEGER32,  32, ATYPE_RW, "PPp_target",    0 },
-    {0x3008, 0x11, DTYPE_UNSIGNED16, 16, ATYPE_RW, "IO_DQ_set",     0 },
     // --> 0x1600
-    {0x1C12, 0x0,  DTYPE_UNSIGNED8,   8, ATYPE_RW, "RxPdoMapCnt",   0 },
-    {0x1C12, 0x1,  DTYPE_UNSIGNED16, 16, ATYPE_RW, "RxPdoMap",      0 },
+    {0x1C12, 0x0,  DTYPE_UNSIGNED8,   8, ATYPE_RW, RxPdoMapCnt.c_str(),   0 },
+    {0x1C12, 0x1,  DTYPE_UNSIGNED16, 16, ATYPE_RW, RxPdoMap.c_str(),      0 },
+    // --> 0x1A00
+    {0x1C13, 0x0,  DTYPE_UNSIGNED8,   8, ATYPE_RW, TxPdoMapCnt.c_str(),   0 },
+    {0x1C13, 0x1,  DTYPE_UNSIGNED16, 16, ATYPE_RW, TxPdoMap.c_str(),      0 },
     //
     {0x1600, 0x0,  DTYPE_UNSIGNED8,   8, ATYPE_RW, "RxElemCnt",     0 },
-    {0x1600, 0x1,  DTYPE_UNSIGNED32, 32, ATYPE_RW, "Rx1",           0 },
-    {0x1600, 0x2,  DTYPE_UNSIGNED32, 32, ATYPE_RW, "Rx2",           0 },
-    {0x1600, 0x3,  DTYPE_UNSIGNED32, 32, ATYPE_RW, "Rx3",           0 },
-    // --> 0x1A00
-    {0x1C13, 0x0,  DTYPE_UNSIGNED8,   8, ATYPE_RW, "TxPdoMapCnt",   0 },
-    {0x1C13, 0x1,  DTYPE_UNSIGNED16, 16, ATYPE_RW, "TxPdoMap",      0 },
+    {0x1600, 0x1,  DTYPE_UNSIGNED32, 32, ATYPE_RW, RPdo1.c_str(),   0 },
+    {0x1600, 0x2,  DTYPE_UNSIGNED32, 32, ATYPE_RW, RPdo2.c_str(),   0 },
+    {0x1600, 0x3,  DTYPE_UNSIGNED32, 32, ATYPE_RW, RPdo3.c_str(),   0 },
+    {0x1600, 0x4,  DTYPE_UNSIGNED32, 32, ATYPE_RW, RPdo4.c_str(),   0 },
+    {0x1600, 0x5,  DTYPE_UNSIGNED32, 32, ATYPE_RW, RPdo5.c_str(),   0 },
+    {0x1600, 0x6,  DTYPE_UNSIGNED32, 32, ATYPE_RW, RPdo6.c_str(),   0 },
+    {0x1600, 0x7,  DTYPE_UNSIGNED32, 32, ATYPE_RW, RPdo7.c_str(),   0 },
+    {0x1600, 0x8,  DTYPE_UNSIGNED32, 32, ATYPE_RW, RPdo8.c_str(),   0 },
     //
     {0x1A00, 0x0,  DTYPE_UNSIGNED8,   8, ATYPE_RW, "TxElemCnt",     0 },
-    {0x1A00, 0x1,  DTYPE_UNSIGNED32, 32, ATYPE_RW, "Tx1",           0 },
-    {0x1A00, 0x2,  DTYPE_UNSIGNED32, 32, ATYPE_RW, "Tx2",           0 },
-    {0x1A00, 0x3,  DTYPE_UNSIGNED32, 32, ATYPE_RW, "Tx3",           0 },
-    {0x1A00, 0x4,  DTYPE_UNSIGNED32, 32, ATYPE_RW, "Tx4",           0 },
+    {0x1A00, 0x1,  DTYPE_UNSIGNED32, 32, ATYPE_RW, TPdo1.c_str(),   0 },
+    {0x1A00, 0x2,  DTYPE_UNSIGNED32, 32, ATYPE_RW, TPdo2.c_str(),   0 },
+    {0x1A00, 0x3,  DTYPE_UNSIGNED32, 32, ATYPE_RW, TPdo3.c_str(),   0 },
+    {0x1A00, 0x4,  DTYPE_UNSIGNED32, 32, ATYPE_RW, TPdo4.c_str(),   0 },
+    {0x1A00, 0x5,  DTYPE_UNSIGNED32, 32, ATYPE_RW, TPdo5.c_str(),   0 },
+    {0x1A00, 0x6,  DTYPE_UNSIGNED32, 32, ATYPE_RW, TPdo6.c_str(),   0 },
+    {0x1A00, 0x7,  DTYPE_UNSIGNED32, 32, ATYPE_RW, TPdo7.c_str(),   0 },
+    {0x1A00, 0x8,  DTYPE_UNSIGNED32, 32, ATYPE_RW, TPdo8.c_str(),   0 },
     
+    // 0x1A00 slave TxPDO 
+    {0x6041, 0x0,  DTYPE_UNSIGNED16, 16, ATYPE_RO, _DCOMstatus.c_str(),   0 },
+    {0x6061, 0x0,  DTYPE_INTEGER8,    8, ATYPE_RO, _DCOMopmd_act.c_str(), 0 },
+    {0x6064, 0x0,  DTYPE_INTEGER32,  32, ATYPE_RO, _p_act.c_str(),        0 },
+    {0x60F4, 0x0,  DTYPE_INTEGER32,  32, ATYPE_RO, _p_dif.c_str(),        0 },
+    {0x6077, 0x0,  DTYPE_INTEGER32,  16, ATYPE_RO, _tq_act.c_str(),        0 },
+    {0x603F, 0x0,  DTYPE_UNSIGNED16, 16, ATYPE_RO, _LastError.c_str(),    0 },
+    {0x3008, 0x1,  DTYPE_UNSIGNED16, 16, ATYPE_RO, _IO_act.c_str(),       0 },    
+    // 0x1600 slave RxPDO 
+    {0x6040, 0x0,  DTYPE_UNSIGNED16, 16, ATYPE_RW, DCOMcontrol.c_str(),   0 },
+    {0x6060, 0x0,  DTYPE_INTEGER8,    8, ATYPE_RW, DCOMopmode.c_str(),    0 },
+    {0x607A, 0x0,  DTYPE_INTEGER32,  32, ATYPE_RW, PPp_target.c_str(),    0 },
+    {0x60FF, 0x0,  DTYPE_INTEGER32,  32, ATYPE_RW, PVv_target.c_str(),    0 },
+    {0x6071, 0x0,  DTYPE_INTEGER16,  16, ATYPE_RW, PTtq_target.c_str(),   0 },
+    {0x3008, 0x11, DTYPE_UNSIGNED16, 16, ATYPE_RW, IO_DQ_set.c_str(),     0 },
+    {0x301B, 0x9,  DTYPE_UNSIGNED16, 16, ATYPE_RW, JOGactivate.c_str(),   0 },
+    
+    {0x6081, 0x0,  DTYPE_UNSIGNED32, 32, ATYPE_RW, PPv_target.c_str(),   0 },
+    {0x6083, 0x0,  DTYPE_UNSIGNED32, 32, ATYPE_RW, RAMP_v_acc.c_str(),   0 },
+    {0x6084, 0x0,  DTYPE_UNSIGNED32, 32, ATYPE_RW, RAMP_v_dec.c_str(),   0 },
+    // position scaling 
+    {0x3006, 0x7,  DTYPE_INTEGER32,  32, ATYPE_RW, ScalePOSdenom.c_str(),0 },
+    {0x3006, 0x8,  DTYPE_INTEGER32,  32, ATYPE_RW, ScalePOSnum.c_str(),  0 },
     
     {0, 0, 0, 0, 0, 0, 0 }
 };
@@ -74,26 +71,43 @@ void LXM32iESC::init_SDOs ( void ) {
 
     memcpy ( ( void* ) SDOs, source_SDOs, sizeof ( source_SDOs ) );
 
-    // 
-    SDOs[i++].data = ( void* ) &LXM32iESC::rx_pdo._DCOMstatus;
-    SDOs[i++].data = ( void* ) &LXM32iESC::rx_pdo._p_act;
-    SDOs[i++].data = ( void* ) &LXM32iESC::rx_pdo._LastError;
-    SDOs[i++].data = ( void* ) &LXM32iESC::rx_pdo._IO_act;
-    // 
-    SDOs[i++].data = ( void* ) &LXM32iESC::tx_pdo.DCOMcontrol;
-    SDOs[i++].data = ( void* ) &LXM32iESC::tx_pdo.PPp_target;
-    SDOs[i++].data = ( void* ) &LXM32iESC::tx_pdo.IO_DQ_set;
     //
     SDOs[i++].data = ( void* ) &LXM32iESC::sdo.rxPdoMapCnt;
     SDOs[i++].data = ( void* ) &LXM32iESC::sdo.rxPdoMap;
+    
     SDOs[i++].data = ( void* ) &LXM32iESC::sdo.rxElemCnt;
-    for(int j=0;j<=2;j++) SDOs[i++].data = ( void* ) &LXM32iESC::sdo.rxMap[j];
+    for(int j=0;j<8;j++) SDOs[i++].data = ( void* ) &LXM32iESC::sdo.rxMap[j];
     
     //
     SDOs[i++].data = ( void* ) &LXM32iESC::sdo.txPdoMapCnt;
     SDOs[i++].data = ( void* ) &LXM32iESC::sdo.txPdoMap;
+    
     SDOs[i++].data = ( void* ) &LXM32iESC::sdo.txElemCnt;
-    for(int j=0;j<=3;j++) SDOs[i++].data = ( void* ) &LXM32iESC::sdo.txMap[j];
+    for(int j=0;j<8;j++) SDOs[i++].data = ( void* ) &LXM32iESC::sdo.txMap[j];
+    
+    // 
+    SDOs[i++].data = ( void* ) &LXM32iESC::rx_pdo._DCOMstatus;
+    SDOs[i++].data = ( void* ) &LXM32iESC::rx_pdo._DCOMopmd_act;
+    SDOs[i++].data = ( void* ) &LXM32iESC::rx_pdo._p_act;
+    SDOs[i++].data = ( void* ) &LXM32iESC::rx_pdo._p_dif;
+    SDOs[i++].data = ( void* ) &LXM32iESC::rx_pdo._tq_act;
+    SDOs[i++].data = ( void* ) &LXM32iESC::rx_pdo._LastError;
+    SDOs[i++].data = ( void* ) &LXM32iESC::rx_pdo._IO_act;
+    // 
+    SDOs[i++].data = ( void* ) &LXM32iESC::tx_pdo.DCOMcontrol;
+    SDOs[i++].data = ( void* ) &LXM32iESC::tx_pdo.DCOMopmode;
+    SDOs[i++].data = ( void* ) &LXM32iESC::tx_pdo.PPp_target;
+    SDOs[i++].data = ( void* ) &LXM32iESC::tx_pdo.PVv_target;
+    SDOs[i++].data = ( void* ) &LXM32iESC::tx_pdo.PTtq_target;
+    SDOs[i++].data = ( void* ) &LXM32iESC::tx_pdo.IO_DQ_set;
+    SDOs[i++].data = ( void* ) &LXM32iESC::tx_pdo.JOGactivate;
+    
+    SDOs[i++].data = ( void* ) &LXM32iESC::sdo.PPv_target;
+    SDOs[i++].data = ( void* ) &LXM32iESC::sdo.RAMP_v_acc;
+    SDOs[i++].data = ( void* ) &LXM32iESC::sdo.RAMP_v_dec;
+    
+    SDOs[i++].data = ( void* ) &LXM32iESC::sdo.ScalePOSdenom;
+    SDOs[i++].data = ( void* ) &LXM32iESC::sdo.ScalePOSnum;
     
     SDOs[i++].data = 0;
 
